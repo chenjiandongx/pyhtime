ed72a0f806b8e4b217f2c1cc299ebc0ec1ff75be
chenjiandongx <chenjiandongx@qq.com>
2018-04-02 18:52:11 +0800

    setup.py 更新

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     version="0.0.1",
     author="chenjiandongx",
     description="一个用来将 datetime 时间转化成更加人性化的`时间描述字符串`",
-    url="https://github.com/chenjiandongx/pytiemago",
+    url="https://github.com/chenjiandongx/pytime",
     author_email="chenjiandongx@qq.com",
     license="MIT",
     py_modules=["pyhtime"],
